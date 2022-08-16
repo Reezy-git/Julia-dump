4960843105e48944591e2834fb10e7151965f4d9
Reezy-git <36226936+Reezy-git@users.noreply.github.com>
2022-08-17 00:00:49 +1200

    Update Snake.jl

--- Snake.jl
+++ Snake.jl
@@ -1,33 +1,43 @@
-
 begin
-    using Makie
+    using GLMakie
 
     res = 500
+    sizea = 10
     game = Scene(resolution = (res, res))
-    limits! = (ax, 0, size, size, size)
-    head =  Observable(Point2(5, 5))
-    scatter!(game, head)
-    size = 10
+    limits! = (game, 0, sizea, sizea, sizea)
+    cell_size = res / sizea + 1
+    head =  Observable(Point2(res/2, res/2))
+    global alive = true
+    scatter!(game, head, markersize=cell_size, marker=:"🟩")
+
+    # 🍎
+    
+    game_matrix = zeros(sizea, sizea)
 
         
     display(game)
 
     global last_dir = (0, 1)
-    dir = lift(game.events.keyboardbuttons) do but
-        global last_dir
-        ispressed(but, Keyboard.left) && return (last_dir = (-1, 0))
-        ispressed(but, Keyboard.up) && return (last_dir = (0, 1))
-        ispressed(but, Keyboard.right) && return (last_dir = (1, 0))
-        ispressed(but, Keyboard.down) && return (last_dir = (0, -1))
-        last_dir
-    end
 
-    display(game)
+    on(events(game).keyboardbutton) do event
+        if event.action in (Keyboard.press, Keyboard.repeat)
+            global last_dir
+            event.key == Keyboard.left && return (last_dir = (-1, 0))
+            event.key == Keyboard.right && return (last_dir = (1, 0))
+            event.key == Keyboard.up && return (last_dir = (0, 1))
+            event.key == Keyboard.down && return (last_dir = (0, -1))
+        end
+        return Consume(false)
+    end
     function move()
-        head[] = head[] .+ Point2(dir)
+        head[] = head[] .+ Point2(last_dir .* cell_size)
     end
+    display(game)
+    campixel!(game)
 
-    while head[][2] < size
-        move()
-    end
 end
+while alive
+    move()
+    sleep(0.5)
+end
+display(game)
